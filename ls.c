78020edea837ad2064e3c92dcaec1427cd9e68d8
Cameron Hudson <chudson@middlebury.edu>
2023-09-21 15:02:29 -0400

    First commit

---
+++ ls.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+// First be able to read the current directory
+//
+// Then be able to read about the files with in the directory 
+//
+// Afterwards know the meaning of the permissions stuff 
+//
+//
+
+int main(int argc, char* argv[]) {
+
+if(argc > 2) {
+
+// print some error with perror and the other stuff
+
+}
+
+if(argv[0] = "ls") {
+
+}
+
+else {
+
+
+}
